33e57b5c94fb1f95194f78d6facf75c567ee45dc
Sandro Jäckel <sandro.jaeckel@sap.com>
2024-02-28 15:41:59 +0100

    Add easy helper to create vault clients

--- go.mod
+++ go.mod
@@ -21,15 +21,32 @@ require (
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v3 v3.0.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.6.6 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/go-secure-stdlib/parseutil v0.1.6 // indirect
+	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hashicorp/vault/api v1.12.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/ryanuber/go-glob v1.0.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 )
---
+++ vault/client.go
@@ -0,0 +1,75 @@
+/******************************************************************************
+*
+*  Copyright 2024 SAP SE
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+******************************************************************************/
+
+package vault
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/hashicorp/vault/api"
+)
+
+// CreateClient creates and returns a vault api client and supports authentication using VAULT_TOKEN, VAULT_ROLE_ID and VAULT_SECRET_ID or ~/.vault-token
+func CreateClient() (*api.Client, error) {
+	cfg := api.DefaultConfig()
+	if cfg.Error != nil {
+		return nil, fmt.Errorf("while reading Vault config from environment: %w", cfg.Error)
+	}
+
+	client, err := api.NewClient(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("while initializing Vault client: %w", err)
+	}
+
+	// always prefer VAULT_TOKEN environment variable
+	if os.Getenv("VAULT_TOKEN") == "" {
+		if os.Getenv("VAULT_ROLE_ID") != "" && os.Getenv("VAULT_SECRET_ID") != "" {
+			//perform app-role authentication if necessary
+			resp, err := client.Logical().Write("auth/approle/login", map[string]interface{}{
+				"role_id":   os.Getenv("VAULT_ROLE_ID"),
+				"secret_id": os.Getenv("VAULT_SECRET_ID"),
+			})
+			if err != nil {
+				return nil, fmt.Errorf("while obtaining approle token: %w", err)
+			}
+			client.SetToken(resp.Auth.ClientToken)
+		} else {
+			homeDir, err := os.UserHomeDir()
+			if err != nil {
+				return nil, fmt.Errorf("while fetching home directory: %w", err)
+			}
+			vaultTokenFile := homeDir + "/.vault-token"
+			if _, err := os.Stat(vaultTokenFile); err == nil {
+				bytes, err := os.ReadFile(vaultTokenFile)
+				if err != nil {
+					return nil, fmt.Errorf("failed reading %s: %w", vaultTokenFile, err)
+				}
+				client.SetToken(strings.TrimSpace(string(bytes)))
+			}
+		}
+	}
+
+	if client.Token() == "" {
+		return nil, errors.New("Some environment variables are missing! For pipelines makes sure VAULT_ROLE_ID and VAULT_SECRET_ID are set and for manual invocations make sure VAULT_TOKEN is set or you previously logged into vault cli. DO NOT use the variables the other way around!") //nolint:stylecheck // we want it like this
+	}
+
+	return client, nil
+}
