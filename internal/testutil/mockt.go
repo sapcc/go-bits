e6e5db9ab72438fd06eeef7d38d7450023bcc3f5
Daniel Wagner <daniel.wagner03@sap.com>
2026-03-24 13:21:18 +0100

    enable revive, run gomm, fix docstring-lints

--- internal/testutil/mockt.go
+++ internal/testutil/mockt.go
@@ -10,13 +10,15 @@ import (
 	"github.com/sapcc/go-bits/assert"
 )
 
-// A mock for *testing.T that implements the assert.TestingT interface.
+// MockT is a mock for *testing.T that implements the assert.TestingT interface.
 type MockT struct {
 	Errors []string
 }
 
+// Helper implements the assert.TestingT interface.
 func (mt *MockT) Helper() {}
 
+// Errorf implements the assert.TestingT interface.
 func (mt *MockT) Errorf(msg string, args ...any) {
 	mt.Errors = append(mt.Errors, fmt.Sprintf(msg, args...))
 }
@@ -29,7 +31,7 @@ func (mt *MockT) ExpectErrors(t *testing.T, expected ...string) {
 	mt.Errors = nil
 }
 
-// ExpectErrors asserts that no errors were collected so far.
+// ExpectNoErrors asserts that no errors were collected so far.
 func (mt *MockT) ExpectNoErrors(t *testing.T) {
 	t.Helper()
 	assert.DeepEqual(t, "collected errors", mt.Errors, []string(nil))
